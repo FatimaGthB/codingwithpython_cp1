2fa756dab9e226a96aec0b1ed1042899a9afed55
FatimaGthB <143875397+FatimaGthB@users.noreply.github.com>
2023-09-04 12:38:09 +0100

    Add files via upload

--- game.py
+++ game.py
@@ -7,14 +7,14 @@ print("Welcome to the Guess the Number game!")
 print("I'm thinking of a number between 1 and 100. Can you guess what it is?")
 
 while nbr_essais > 0 :
-    nbr_essais -= 1 # le nombre d'essai possible sera réduit de 1 pour chaque entrée de l'utilisateur
-    var = int(input("Enter your guess: ")) # var sera le nombre entré par l'utilisateur
+    nbr_essais -= 1      # le nombre d'essai possible sera réduit de 1 pour chaque entrée de l'utilisateur
+    var = int(input("Enter your guess: "))         # var sera le nombre entré par l'utilisateur
     if var > n :
         print("Your guess is too high. Guess again.")
     elif var < n :
         print("Your guess is too low. Guess again.")
     else :
-        break
+        break                    # on sort de la boucle while
 if nbr_essais != 0 :
     print("Congratulations! You guessed the number correctly in " ,5-nbr_essais, "tries !")
 else :
